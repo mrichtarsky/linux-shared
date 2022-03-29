9c9debe7c052b513205cc0152fb16ce362294c10
Martin Richtarsky <s@martinien.de>
2022-03-29 15:16:40 +0200

    git delta

--- install_packages.sh
+++ install_packages.sh
@@ -6,7 +6,12 @@ DISTRO=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
 echo $DISTRO
 if [ "$DISTRO" == '"sles"' ];
 then
-    sudo zypper install $PACKAGES
+    sudo zypper install $PACKAGES git-delta
 else
     sudo apt install $PACKAGES
+    pushd /tmp
+    wget https://github.com/dandavison/delta/releases/download/0.12.1/git-delta_0.12.1_amd64.deb
+    dpkg -i git-delta_0.12.1_amd64.deb
+    rm git-delta_0.12.1_amd64.deb
+    popd
 fi
