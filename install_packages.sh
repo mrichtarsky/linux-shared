8508c06bb2e7a3d062b954b28f00774ea4516e6a
Martin Richtarsky <martin.richtarsky@sap.com>
2022-04-08 13:02:35 +0200

    Fix package install

--- install_packages.sh
+++ install_packages.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PACKAGES="mc tmux htop iostat ncdu git ripgrep python3"
+PACKAGES="mc tmux htop ncdu git ripgrep python3"
 DISTRO=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
 echo $DISTRO
 if [ "$DISTRO" == '"sles"' ];
 then
-    sudo zypper install $PACKAGES git-delta
+    sudo zypper install $PACKAGES sysstat
+    pushd /tmp
+    wget https://github.com/dandavison/delta/releases/download/0.12.1/delta-0.12.1-x86_64-unknown-linux-gnu.tar.gz
+    tar xf delta-0.12.1-x86_64-unknown-linux-gnu.tar.gz
+    rm delta-0.12.1-x86_64-unknown-linux-gnu.tar.gz
+    popd
 else
-    sudo apt install $PACKAGES
+    sudo apt install $PACKAGES iostat
     pushd /tmp
     wget https://github.com/dandavison/delta/releases/download/0.12.1/git-delta_0.12.1_amd64.deb
     sudo dpkg -i git-delta_0.12.1_amd64.deb
     rm git-delta_0.12.1_amd64.deb
-    wget https://dystroy.org/broot/download/x86_64-linux/broot
-    chmod a+x broot
-    sudo mv broot /usr/local/bin/broot
     popd
 fi
+
+wget https://dystroy.org/broot/download/x86_64-linux/broot
+chmod a+x broot
+sudo mv broot /usr/local/bin/broot
