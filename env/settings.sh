daf630559702ab4e82c938017f85a553a602f383
Martin Richtarsky <s@martinien.de>
2022-08-09 22:42:42 +0200

    Fix VS code shell integration error msg

--- env/settings.sh
+++ env/settings.sh
@@ -10,7 +10,8 @@ export HISTFILESIZE=100000               # big big history
 shopt -s histappend                      # append to history, don't overwrite it
 
 # Save and reload the history after each command finishes
-export PROMPT_COMMAND="history -a; history -c; history -r; ${PROMPT_COMMAND-}"
+export PROMPT_COMMAND="history -a; history -c; history -r"
+# VS Code shell integration breaks this; ${PROMPT_COMMAND-}"
 
 # Default debian color prompt
 #export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
