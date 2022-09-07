b644a1f3a68ff956e1def1e6b7bdf03633f5d442
Martin Richtarsky <s@martinien.de>
2022-09-07 11:20:17 +0200

    source rust before using it

--- _init.sh
+++ _init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #set -euox pipefail
 
-source /r/env/settings.sh
-source /r/env/aliases.sh
-source /r/env/functions.sh
-
 export RUSTUP_HOME=/p/tools/rust
 export CARGO_HOME=/p/tools/rust/.cargo
 source /p/tools/rust/.cargo/env
+
+source /r/env/settings.sh
+source /r/env/aliases.sh
+source /r/env/functions.sh
