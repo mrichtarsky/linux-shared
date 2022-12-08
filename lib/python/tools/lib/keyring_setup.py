d69b3875ee4a5b36eec0074f5331746ac40830fc
Martin Richtarsky <s@martinien.de>
2022-12-08 11:12:17 +0100

    Do not delete KEY for now
    Breaks subprocesses

--- lib/python/tools/lib/keyring_setup.py
+++ lib/python/tools/lib/keyring_setup.py
@@ -13,7 +13,6 @@ keyring.util.platform_.data_root = lambda: KEYRING_DIR
 keyring = CryptFileKeyring()
 try:
     passphrase = os.environ['KEY']
-    del os.environ['KEY']
 except KeyError:
     if 'PS1' in os.environ:  # interactive shell, prompt for key
         passphrase = getpass.getpass('Passphrase not found in KEY environment, please enter: ')
