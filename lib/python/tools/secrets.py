56c711badb88876b317e2f73f47fc8ad1978fa54
Martin Richtarsky <s@martinien.de>
2023-09-12 09:03:46 +0200

    Improve import performance

--- lib/python/tools/secrets.py
+++ lib/python/tools/secrets.py
@@ -4,8 +4,6 @@ import sys
 
 from prodict import Prodict  # pylint: disable=syntax-error
 
-from tools.lib.keyring_setup import keyring
-
 SECRETS_PATH = pathlib.Path('/repos/secrets')
 TOKEN_CACHE_PATH = SECRETS_PATH / 'token_cache'
 
@@ -48,7 +46,7 @@ class LazyInfos:
     def __getattr__(self, name):
         value = self.items[name]
         if callable(value):
-            value = value()
+            value = value()  # Cache return value
             if value is None:
                 raise Exception('Returned None')
         return value
@@ -57,14 +55,18 @@ class LazyInfos:
         return "\n".join(f"{key} = {self.__getattr__(key)}" for key in sorted(self.items))
 
 def addCredential(system, user, secretAttributes=(), extraAttributes=None):
+    def getPasswordWithImport(system, attribute):
+        from tools.lib.keyring_setup import keyring
+        return keyring.get_password(system, attribute)
+
     if system in credentials:
         raise Exception('Duplicate system')
     info = credentials[system] = LazyInfos()
     info.add('user', user)
-    info.add('password', lambda: keyring.get_password(system, user))
+    info.add('password', lambda: getPasswordWithImport(system, user))
     for secretAttribute in secretAttributes:
         info.add(secretAttribute,
-                 Callable(keyring.get_password, system, secretAttribute))
+                 Callable(getPasswordWithImport, system, secretAttribute))
     if extraAttributes:
         for name, value in extraAttributes.items():
             info.add(name, value)
