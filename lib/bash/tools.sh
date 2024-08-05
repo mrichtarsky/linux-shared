9e617fb9f8140c4f8dcb461edde5ae21bc4437bc
Martin Richtarsky <s@martinien.de>
2024-08-05 09:59:57 +0000

    rsync: use --archive

--- lib/bash/tools.sh
+++ lib/bash/tools.sh
@@ -23,11 +23,13 @@ add_cronjob() {
 }
 
 rsync_common() {
-    rsync --compress --recursive --links --perms --executability --times --delete --delete-excluded --stats --info=progress2 --human-readable --fake-super "$@"
+    # --executability: retains executability even if umask would remove it
+    rsync --archive --executability --compress --delete --delete-excluded --stats --info=progress2 --human-readable --fake-super "$@"
 }
 
 rsync_common_nodelete() {
-    rsync --compress --recursive --links --perms --executability --times --stats --info=progress2 --human-readable --fake-super "$@"
+    # --executability: retains executability even if umask would remove it
+    rsync --archive --executability --compress --stats --info=progress2 --human-readable --fake-super "$@"
 }
 
 # This is safer than just using 'ls -1t'
